cc0cc59df339173eb3fa36ecd1a84ec6709b2384
tonysargent <109193497+tonysargent@users.noreply.github.com>
2022-10-31 21:16:01 +1100

    files added
    Files added to repository

---
+++ index.js
@@ -0,0 +1,42 @@
+function App(){
+  const [todos, setTodos] = React.useState([
+    {
+      text: 'Wake Up',
+      isCompleted: false,
+    },
+    {
+      text: 'Get Hugo Breakfast',
+      isCompleted: false,
+    },
+    {
+      text: 'Ride bike over the mountain',
+      isCompleted: false,
+    }        
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, {text, isCompleted:false}];
+    setTodos(newTodos);
+  }
+  const removeTodo = index => {
+    let temp = [...todos];    
+    temp.splice(index, 1);
+    setTodos(temp);
+  }
+
+  return(
+    <div className="app">
+      <div className="todo-list" >
+        {todos.map((todo, i) => (
+          <Todo key={i} index={i} todo={todo} remove={removeTodo}/>
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
